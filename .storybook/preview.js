0c619902f0729777dbf8979daffd2f5e711a2f9e
Diego Morales <diego.morales@gmx.net>
2024-09-12 20:58:43 +0200

    update storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,15 @@
 import '../src/main.css'
+import * as Components from '../src/main.js'
+
+for (let c in Components) {
+  Components[c].define()
+}
 
 /** @type { import('@storybook/html').Preview } */
 const preview = {
   parameters: {
     controls: {
+      disableSaveFromUI: true,
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
