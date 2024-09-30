625a57a7b67770d0f1dbfb4e7316f82a07861702
Diego Morales <diego.morales@gmx.net>
2024-10-01 00:05:21 +0200

    update storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,9 @@
 import '../src/main.css'
 import * as Components from '../src/main.js'
+import beautify from 'js-beautify'
+
+import prettier from 'prettier/standalone.mjs'
+import htmlParser from 'prettier/plugins/html.mjs'
 
 for (let c in Components) {
   Components[c].define()
@@ -16,6 +20,19 @@ const preview = {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
+      },
+    },
+    docs: {
+      source: {
+        transform: (input) => {
+          return beautify.html(input, {
+            indent_size: 4,
+            wrap_line_length: 80,
+            preserve_newlines: false,
+            brace_style: 'expand',
+            wrap_attributes: 'force',
+          })
+        },
       },
     },
   },
